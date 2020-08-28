c6251fd90b768d3a19d9d12b1ddbdbbbe933f051
kamrul3 <kamrulman55@gmail.com>
2020-08-28 15:34:24 +0600

    mani changes

---
+++ js/script.js
@@ -0,0 +1,376 @@
+
+
+  const spyscrll = ()=>{
+    const section = document.querySelectorAll("section")
+    window.onscroll = ()=>{
+      const sposition = document.documentElement.scrollTop || document.body.scrollTop
+      section.forEach((item)=>{
+       if(item.offsetTop <= sposition){
+         const id = item.id
+        //  document.querySelector(".active").classList.remove("active")
+        //  document.querySelector(`a[href*=${id}]`).parentNode.classList.add("active")
+        
+       }
+      })
+    }
+  }
+  spyscrll(),
+
+
+$(window).scroll(function(){
+  var scrollamount = $(window).scrollTop()
+  if(scrollamount > 10){
+    $(".menu").addClass("fixed")
+  }else{
+    $(".menu").removeClass("fixed")
+  }
+  if(scrollamount > 500){
+    $(".btop").fadeIn();
+  }else{
+    $(".btop").fadeOut();
+  }
+})
+$(".btop").click(function(){
+$("html,body").animate({
+  scrollTop:0
+},1000)
+}),
+
+
+
+
+//  =================== banner slider start ==========================
+$('#bannnerPart').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    prevArrow: '<i class="fas fa-arrow-left bnrprev"></i>',
+    nextArrow: '<i class="fas fa-arrow-right bnrnext"></i>',
+    fade: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  //  =================== banner slider end ==========================
+
+
+  $('.pslide').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '0px',
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+//  =================== vanu box start ==========================
+
+  $('.venobox').venobox();
+
+//  =================== vanu box end ============================
+
+//  =================== service slider start ==========================
+  $('.slideup').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fas fa-chevron-up srvcup"></i>',
+    nextArrow: '<i class="fas fa-chevron-down srvcdown"></i>',
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          vertical: true,
+          verticalSwiping: true,
+          centerMode: true,
+          centerPadding: '0px',
+          focusOnSelect: true,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+//  =================== service slider end ==========================
+//  ===================  testimonial slider start ====================
+
+  $('.imgslide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fas fa-chevron-up tup"></i>',
+    nextArrow: '<i class="fas fa-chevron-down tdown"></i>',
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+    asNavFor: '.textslider',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          vertical: false,
+          verticalSwiping: false,
+          centerMode: true,
+          centerPadding: '0px',
+          focusOnSelect: true,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          vertical: false,
+          verticalSwiping: false,
+          centerMode: true,
+          centerPadding: '0px',
+          focusOnSelect: true,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.textslider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.imgslide',
+    dots: false,
+    // vertical: true,
+    // verticalSwiping: true,
+    // centerMode: true,
+    // centerPadding: '0px',
+    fade: true,
+    speed: 1000,
+    focusOnSelect: true,
+    arrows: false,
+  });
+//  ===================  testimonial slider end ====================
+// ============ counter up==============
+
+let counter = document.querySelectorAll(".counter")
+let counterarr = Array.from(counter)
+counterarr.map((item)=>{
+  let i = 0
+  let endnumber = item.dataset.number
+  let speed = item.dataset.speed
+  function counterjs() {
+   item.innerHTML = i
+   if (i == endnumber) {
+       clearInterval(stop)
+   }
+  }
+  let stop = setInterval(() => {
+      i++
+    counterjs()
+  }, speed);
+})
+// ==================counter end====================
+
+// ==================team prat start====================
+$('.member').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  // autoplay: true,
+  // autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// ==================team part end====================
+
+// ==================add part start====================
+$('.aditem').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: false,
+  centerMode: true,
+  centerPadding: '0px',
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// ==================add part end====================
+
+
+
+
+
+
+
+
+
+
